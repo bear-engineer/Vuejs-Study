c2779e034c82ff0fc01ecb5feaf120c708eba6e9
Sehyeon Youn <dev.younlab@gmail.com>
2019-04-21 22:28:47 +0900

    [ADD] vue-news / setting / vuex api / vuter

---
+++ vue-news/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <!-- Essential Recommended -->
+    <tool-bar></tool-bar>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+import ToolBar from "./components/ToolBar.vue";
+export default {
+  components: {
+    ToolBar
+  }
+};
+</script>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ vue-news/src/api/index.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+// 1. HTTP Request & Response
+const config = {
+  baseUrl: "https://api.hnpwa.com/v0/"
+};
+
+// 2. API 함수들을 정리
+const fetchNewsList = () => {
+  return axios.get(`${config.baseUrl}news/1.json`);
+};
+
+const fetchJobsList = () => {
+  return axios.get(`${config.baseUrl}jobs/1.json`);
+};
+
+const fetchAskList = () => {
+  return axios.get(`${config.baseUrl}ask/1.json`);
+};
+
+export { fetchNewsList, fetchJobsList, fetchAskList };
