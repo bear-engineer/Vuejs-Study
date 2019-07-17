a73e44ca659a13573d7ae468d517ac91f00b452d
Sehyeon Youn <dev.younlab@gmail.com>
2019-07-17 23:38:50 +0900

    [ADD] login form test end

---
+++ vue-cli-basic/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <div>
+      <label for="username">id:</label>
+      <input type="text" id="username" v-model="username" />
+    </div>
+    <div>
+      <label for="password">password:</label>
+      <input type="password" id="password" v-model="password" />
+    </div>
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    onSubmit() {
+      const url = "https://jsonplaceholder.typicode.com/users";
+      const data = {
+        username: this.username,
+        password: this.password
+      };
+      axios
+        .post(url, data)
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
