6d2aee19dbd722eded46fa7e1bc6b6b8c08db9c3
Sehyeon Youn <dev.younlab@gmail.com>
2019-07-18 11:09:43 +0900

    [ADD] router

---
+++ vue-news-re/src/router/index.js
@@ -0,0 +1,25 @@
+import VueRouter from 'vue-router';
+import Vue from 'vue';
+import AskView from '../views/AskView.vue';
+import NewsView from '../views/NewsView.vue';
+import JobsView from '../views/JobsView.vue';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/news',
+      component: NewsView,
+    },
+    {
+      path: '/ask',
+      component: AskView,
+    },
+    {
+      path: '/jobs',
+      component: JobsView,
+    },
+  ],
+});
